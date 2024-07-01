92166b50d98731b676635955b1f2d32ae4103f86
SelfwareEngineer <126501922+SelfwareEngineer@users.noreply.github.com>
2024-07-01 14:33:46 -0400

    Add "Change grid size" button

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
 const content = document.querySelector("#content");
+const gridContainer = document.querySelector("#gridContainer");
+let gridSize = 16;
 
-createGrid(16);
+createGrid(gridSize);
 
 function createGrid(gridSize) {
+  gridSize = gridSize ** 2;
   for (let i = 1; i <= gridSize; i++) {
     let gridSquare = document.createElement("div");
     gridSquare.classList.add("gridSquare");
     gridSquare.id = `gridSquare${i}`;
     gridSquare.style.width = `${100 / Math.sqrt(gridSize)}%`;
     gridSquare.style.height = gridSquare.style.width;
-    content.appendChild(gridSquare);
+    gridContainer.appendChild(gridSquare);
   }
+}
+
+function getNewGridSize(prompt_msg = "Enter a new grid size (max 100)") {
+  let newGridSize = Number(prompt(prompt_msg));
+
+  if (isNaN(newGridSize)) {
+    newGridSize = getNewGridSize("Please enter a number.");
+  } else if (newGridSize > 100) {
+    newGridSize = getNewGridSize("Please enter a number less than 100");
+  }
+  return newGridSize;
 }
 
 document.addEventListener("mouseover", (e) => {
   if (e.target.classList.contains("gridSquare")) {
     e.target.style.backgroundColor = "black";
+  }
+});
+
+document.addEventListener("click", (e) => {
+  if (e.target.id === "changeGridSize") {
+    const newGridSize = getNewGridSize();
+
+    while (gridContainer.lastChild) {
+      gridContainer.removeChild(gridContainer.lastChild);
+    }
+
+    createGrid(newGridSize);
   }
 });
